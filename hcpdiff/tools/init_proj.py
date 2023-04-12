063b0ffd72c7395bc4a0bad186f23fb82cc1153c
dzy7e <228218809@qq.com>
2023-04-12 15:32:12 +0800

    pypi

--- hcpdiff/tools/init_proj.py
+++ hcpdiff/tools/init_proj.py
@@ -3,5 +3,9 @@ import shutil
 import os
 
 def main():
-    shutil.copytree(os.path.join(sys.prefix, 'hcpdiff/cfgs'), r'./cfgs')
-    shutil.copytree(os.path.join(sys.prefix, 'hcpdiff/prompt_tuning_template'), r'./prompt_tuning_template')
+    try:
+        shutil.copytree(os.path.join(sys.prefix, 'hcpdiff/cfgs'), r'./cfgs')
+        shutil.copytree(os.path.join(sys.prefix, 'hcpdiff/prompt_tuning_template'), r'./prompt_tuning_template')
+    except:
+        shutil.copytree(os.path.join(sys.prefix, '../hcpdiff/cfgs'), r'./cfgs')
+        shutil.copytree(os.path.join(sys.prefix, '../hcpdiff/prompt_tuning_template'), r'./prompt_tuning_template')
