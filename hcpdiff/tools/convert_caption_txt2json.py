804d078d7410488dcdddbfd828505caa50d23957
dzy7e <228218809@qq.com>
2023-04-12 16:16:59 +0800

    cap txt2json

---
+++ hcpdiff/tools/convert_caption_txt2json.py
@@ -0,0 +1,25 @@
+import argparse
+import json
+import os
+
+from hcpdiff.utils.img_size_tool import types_support
+
+parser = argparse.ArgumentParser(description='Stable Diffusion Training')
+parser.add_argument('--data_root', type=str, default='')
+args = parser.parse_args()
+
+
+def get_txt_caption(path):
+    with open(path, encoding='utf8') as f:
+        return f.read().strip()
+
+
+captions = {}
+for file in os.listdir(args.data_root):
+    ext_idx = file.rfind('.')
+    file_name = file[:ext_idx]
+    if file[ext_idx + 1:] in types_support:
+        captions[file] = get_txt_caption(os.path.join(args.data_root, f'{file_name}.txt'))
+
+with open(os.path.join(args.data_root, f'image_captions.json'), "w", encoding='utf8') as f:
+    json.dump(captions, f)
