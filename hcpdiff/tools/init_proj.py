5c12fd5617c5a0028b215029c9b11e2117096195
dzy7e <228218809@qq.com>
2023-04-12 15:54:52 +0800

    pypi

--- hcpdiff/tools/init_proj.py
+++ hcpdiff/tools/init_proj.py
@@ -4,6 +4,10 @@ import os
 
 def main():
     try:
+        if os.path.exists(r'./cfgs'):
+            shutil.rmtree(r'./cfgs')
+        if os.path.exists(r'./prompt_tuning_template'):
+            shutil.rmtree(r'./prompt_tuning_template')
         shutil.copytree(os.path.join(sys.prefix, 'hcpdiff/cfgs'), r'./cfgs')
         shutil.copytree(os.path.join(sys.prefix, 'hcpdiff/prompt_tuning_template'), r'./prompt_tuning_template')
     except:
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def get_data_files(data_dir, prefix=''):
 setuptools.setup(
     name="hcpdiff",
     py_modules=["hcpdiff"],
-    version="0.1.2",
+    version="0.1.3",
     author="Ziyi Dong",
     author_email="dzy7eu7d7@gmail.com",
     description="A universal Stable-Diffusion toolbox",
