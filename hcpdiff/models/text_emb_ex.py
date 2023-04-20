faf8bb82f95458faa16606fe95068ecf20220f08
dzy7e <228218809@qq.com>
2023-04-20 15:12:26 +0800

    warp te unet to fix issue #7

--- hcpdiff/models/text_emb_ex.py
+++ hcpdiff/models/text_emb_ex.py
@@ -27,10 +27,11 @@ class EmbeddingPTHook(SinglePluginBlock):
         self.N_word=N_word
         self.N_repeats=N_repeats
         self.num_embeddings=token_embedding.num_embeddings
-        self.emb=nn.ParameterDict()
+        self.emb={}
+        self.emb_train=nn.ParameterList()
 
     def add_emb(self, emb:nn.Parameter, token_id:int):
-        self.emb[str(token_id)]=emb
+        self.emb[token_id]=emb
 
     def pre_hook(self, host, input_ids: Tuple[torch.Tensor]):
         self.input_ids = rearrange(input_ids[0], '(b r) w -> b (r w)', r=self.N_repeats)  # 兼容Attention mask
@@ -54,7 +55,7 @@ class EmbeddingPTHook(SinglePluginBlock):
             for rep_idx in rep_idxs:
                 rep_idx=rep_idx.item()
                 item_new.append(item[rep_idx_last:rep_idx, :])
-                item_new.append(self.emb[str(ids_raw[rep_idx].item())].to(dtype=item.dtype))
+                item_new.append(self.emb[ids_raw[rep_idx].item()].to(dtype=item.dtype))
                 rep_idx_last=rep_idx+1
             item_new.append(item[rep_idx_last:, :])
 
@@ -86,6 +87,6 @@ class EmbeddingPTHook(SinglePluginBlock):
 
     @classmethod
     def hook_from_dir(cls, emb_dir, tokenizer, text_encoder, log=True, device='cuda:0', **kwargs):
-        ex_words_emb = {file[:-3]: nn.Parameter(load_emb(os.path.join(emb_dir, file)).to(device), requires_grad=True)
+        ex_words_emb = {file[:-3]: nn.Parameter(load_emb(os.path.join(emb_dir, file)).to(device), requires_grad=False)
                         for file in os.listdir(emb_dir) if file.endswith('.pt')}
         return cls.hook(ex_words_emb, tokenizer, text_encoder, log, **kwargs), ex_words_emb
--- hcpdiff/train_ac.py
+++ hcpdiff/train_ac.py
@@ -109,7 +109,7 @@ class Trainer:
         return self.accelerator.is_local_main_process
 
     def init_context(self, cfgs_raw):
-        ddp_kwargs = DistributedDataParallelKwargs(broadcast_buffers=False, find_unused_parameters=True)
+        ddp_kwargs = DistributedDataParallelKwargs(broadcast_buffers=False)
         self.accelerator = Accelerator(
             gradient_accumulation_steps=self.cfgs.train.gradient_accumulation_steps,
             mixed_precision=self.cfgs.mixed_precision,
@@ -298,6 +298,7 @@ class Trainer:
             for v in self.cfgs.tokenizer_pt.train:
                 self.train_pts[v.name]=self.ex_words_emb[v.name]
                 self.ex_words_emb[v.name].requires_grad=True
+                self.embedding_hook.emb_train.append(self.ex_words_emb[v.name])
                 train_params_emb.append({'params':self.ex_words_emb[v.name], 'lr':v.lr})
 
         return train_params_unet + train_params_text_encoder, train_params_emb
@@ -469,7 +470,8 @@ class Trainer:
         return loss.item()
 
     def get_loss(self, model_pred, target, att_mask):
-        return (self.criterion(model_pred.float(), target.float()) * att_mask).mean()
+        return (self.criterion(model_pred.float(), target.float()) * att_mask).mean() \
+               + 0*sum(self.embedding_hook.emb_train).mean() # avoid unused parameters, make gradient checkpointing happy
 
     def update_ema(self):
         if hasattr(self, 'ema_unet'):
