20db7b2c8c2a0508c462dae5dc2994b8b7a2ce6d
dzy7e <228218809@qq.com>
2023-05-06 19:45:56 +0800

    hcpinit fix

--- hcpdiff/tools/init_proj.py
+++ hcpdiff/tools/init_proj.py
@@ -2,15 +2,17 @@ import sys
 import shutil
 import os
 
-
 def main():
+    prefix = sys.prefix
+    if not os.path.exists(os.path.join(prefix, 'hcpdiff')):
+        prefix = os.path.join(prefix, 'local')
     try:
         if os.path.exists(r'./cfgs'):
             shutil.rmtree(r'./cfgs')
         if os.path.exists(r'./prompt_tuning_template'):
             shutil.rmtree(r'./prompt_tuning_template')
-        shutil.copytree(os.path.join(sys.prefix, 'hcpdiff/cfgs'), r'./cfgs')
-        shutil.copytree(os.path.join(sys.prefix, 'hcpdiff/prompt_tuning_template'), r'./prompt_tuning_template')
+        shutil.copytree(os.path.join(prefix, 'hcpdiff/cfgs'), r'./cfgs')
+        shutil.copytree(os.path.join(prefix, 'hcpdiff/prompt_tuning_template'), r'./prompt_tuning_template')
     except:
-        shutil.copytree(os.path.join(sys.prefix, '../hcpdiff/cfgs'), r'./cfgs')
-        shutil.copytree(os.path.join(sys.prefix, '../hcpdiff/prompt_tuning_template'), r'./prompt_tuning_template')
+        shutil.copytree(os.path.join(prefix, '../hcpdiff/cfgs'), r'./cfgs')
+        shutil.copytree(os.path.join(prefix, '../hcpdiff/prompt_tuning_template'), r'./prompt_tuning_template')
