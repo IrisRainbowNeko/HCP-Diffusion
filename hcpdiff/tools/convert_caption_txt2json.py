9db879a28d2cf3ad615d127cfe7cb7b370b92561
dzy7e <228218809@qq.com>
2023-05-07 14:11:13 +0800

    update

--- hcpdiff/tools/convert_caption_txt2json.py
+++ hcpdiff/tools/convert_caption_txt2json.py
@@ -6,6 +6,7 @@ from hcpdiff.utils.img_size_tool import types_support
 
 parser = argparse.ArgumentParser(description='Stable Diffusion Training')
 parser.add_argument('--data_root', type=str, default='')
+parser.add_argument('--with_imgs', action="store_true")
 args = parser.parse_args()
 
 
@@ -16,10 +17,13 @@ def get_txt_caption(path):
 
 captions = {}
 for file in os.listdir(args.data_root):
-    ext_idx = file.rfind('.')
-    file_name = file[:ext_idx]
-    if file[ext_idx + 1:] in types_support:
-        captions[file] = get_txt_caption(os.path.join(args.data_root, f'{file_name}.txt'))
+    file_name, file_ext = file.rsplit('.', 1)
+    if args.with_imgs:
+        if file_ext in types_support:
+            captions[file] = get_txt_caption(os.path.join(args.data_root, f'{file_name}.txt'))
+    else:
+        if file_ext == 'txt':
+            captions[file] = get_txt_caption(os.path.join(args.data_root, f'{file_name}.txt'))
 
 with open(os.path.join(args.data_root, f'image_captions.json'), "w", encoding='utf8') as f:
     json.dump(captions, f, indent=2, ensure_ascii=False)
