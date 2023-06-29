8ba337b0a9df10913de273ae0ea34a61d18157c9
dzy7e <228218809@qq.com>
2023-06-29 10:48:02 +0800

    update

--- hcpdiff/utils/pipe_hook.py
+++ hcpdiff/utils/pipe_hook.py
@@ -123,7 +123,7 @@ class HookPipe_T2I(StableDiffusionPipeline):
                 if i == len(timesteps)-1 or ((i+1)>num_warmup_steps and (i+1)%self.scheduler.order == 0):
                     progress_bar.update()
                     if callback is not None and i%callback_steps == 0:
-                        callback(i, t, timesteps, latents_x0)
+                        callback(i, t, num_inference_steps, latents_x0)
 
         if not output_type == "latent":
             image = self.vae.decode(latents / self.vae.config.scaling_factor, return_dict=False)[0]
@@ -246,7 +246,7 @@ class HookPipe_I2I(StableDiffusionImg2ImgPipeline):
                 if i == len(timesteps)-1 or ((i+1)>num_warmup_steps and (i+1)%self.scheduler.order == 0):
                     progress_bar.update()
                     if callback is not None and i%callback_steps == 0:
-                        callback(i, t, timesteps, latents_x0)
+                        callback(i, t, num_inference_steps, latents_x0)
 
         if not output_type == "latent":
             image = self.vae.decode(latents / self.vae.config.scaling_factor, return_dict=False)[0]
@@ -391,7 +391,7 @@ class HookPipe_Inpaint(StableDiffusionInpaintPipelineLegacy):
                 if i == len(timesteps)-1 or ((i+1)>num_warmup_steps and (i+1)%self.scheduler.order == 0):
                     progress_bar.update()
                     if callback is not None and i%callback_steps == 0:
-                        callback(i, t, timesteps, latents_x0)
+                        callback(i, t, num_inference_steps, latents_x0)
 
         # use original latents corresponding to unmasked portions of the image
         latents = (init_latents_orig*mask)+(latents*(1-mask))
