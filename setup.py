9094e60e4af144b5b5242998c100d2226bae0e19
dzy7e <228218809@qq.com>
2023-07-16 12:03:47 +0800

    update

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def get_data_files(data_dir, prefix=''):
 setuptools.setup(
     name="hcpdiff",
     py_modules=["hcpdiff"],
-    version="0.5.0",
+    version="0.5.1",
     author="Ziyi Dong",
     author_email="dzy7eu7d7@gmail.com",
     description="A universal Stable-Diffusion toolbox",
