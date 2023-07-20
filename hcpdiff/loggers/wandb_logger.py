6db23295f1657b506cf15959d9a92e2deaafa61b
7eu7d7 <228218809@qq.com>
2023-07-20 16:13:22 +0800

    optimize wandb logger

--- hcpdiff/loggers/wandb_logger.py
+++ hcpdiff/loggers/wandb_logger.py
@@ -7,8 +7,8 @@ from .base_logger import BaseLogger
 
 
 class WanDBLogger(BaseLogger):
-    def __init__(self, exp_dir, out_path, enable_log_image=True, project='hcp-diffusion'):
-        super().__init__(exp_dir, out_path, enable_log_image)
+    def __init__(self, exp_dir, out_path=None, enable_log_image=True, project='hcp-diffusion', log_step=10):
+        super().__init__(exp_dir, out_path, enable_log_image, log_step)
         if exp_dir is not None:  # exp_dir is only available in local main process
             wandb.init(project=project)
         else:
@@ -19,7 +19,9 @@ class WanDBLogger(BaseLogger):
         pass
 
     def _log(self, datas: Dict[str, Any], step: int = 0):
-        wandb.log({k: v['data'] for k, v in datas.items()}, step=step)
+        for k, v in datas.items():
+            if len(v['data']) == 1:
+                self.writer.add_scalar(k, v['data'][0], global_step=step)
 
     def _log_image(self, imgs: Dict[str, Image.Image], step: int = 0):
         wandb.log({next(iter(imgs.keys())): list(imgs.values())}, step=step)
