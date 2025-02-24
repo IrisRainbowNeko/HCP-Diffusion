c9865dbc2827a6ea683801d7ea7f7fb9105eb226
IrisRainbowNeko <228218809@qq.com>
2025-02-24 17:45:05 +0800

    fix vae cahce

--- hcpdiff/data/__init__.py
+++ hcpdiff/data/__init__.py
@@ -1,3 +1,4 @@
 from .dataset import TextImagePairDataset
 from .source import Text2ImageSource, Text2ImageLossMapSource, Text2ImageCondSource, T2IFolderClassSource
 from .handler import StableDiffusionHandler, LossMapHandler, DiffusionImageHandler
+from .cache import VaeCache
