5099c419978422356f3cc2544b2e8348cb532409
IrisRainbowNeko <228218809@qq.com>
2025-03-13 16:28:47 +0800

    workflow update

--- hcpdiff/workflow/daam/act.py
+++ hcpdiff/workflow/daam/act.py
@@ -1,33 +1,29 @@
 import os
 from io import BytesIO
-from typing import List
 
 import numpy as np
 from PIL import Image
-from matplotlib import pyplot as plt
-
 from hcpdiff.utils import to_validate_file
 from hcpdiff.utils.img_size_tool import types_support
+from matplotlib import pyplot as plt
+from rainbowneko.infer import BasicAction, Actions
+
 from .hook import DiffusionHeatMapHooker
-from ..base import ContainerAction, BasicAction, feedback_input
 
-class CaptureCrossAttnAction(ContainerAction):
-    def __init__(self, actions: List[BasicAction]):
-        super().__init__(actions)
-
-    @feedback_input
-    def forward(self, memory, prompt, **states):
+class CaptureCrossAttnAction(Actions):
+    def forward(self, prompt, unet, tokenizer, vae, **states):
         bs = len(prompt)
         N_head = 8
-        with DiffusionHeatMapHooker(memory.unet, memory.tokenizer, vae_scale_factor=memory.vae.vae_scale_factor) as tc:
-            states = self.inner_forward(memory, **states)
+        with DiffusionHeatMapHooker(unet, tokenizer, vae_scale_factor=vae.vae_scale_factor) as tc:
+            states = super().forward(**states)
             heat_maps = [tc.compute_global_heat_map(prompt=prompt[i], head_idxs=range(N_head*i, N_head*(i+1))) for i in range(bs)]
 
         return {**states, 'cross_attn_heat_maps':heat_maps}
 
 class SaveWordAttnAction(BasicAction):
 
-    def __init__(self, save_root: str, N_col: int = 4, image_type: str = 'png', quality: int = 95):
+    def __init__(self, save_root: str, N_col: int = 4, image_type: str = 'png', quality: int = 95, key_map_in=None, key_map_out=None):
+        super().__init__(key_map_in, key_map_out)
         self.save_root = save_root
         self.image_type = image_type
         self.quality = quality
@@ -60,12 +56,11 @@ class SaveWordAttnAction(BasicAction):
         buf.seek(0)
         return Image.open(buf)
 
-    @feedback_input
-    def forward(self, memory, images, prompt, seeds, cross_attn_heat_maps, **states):
+    def forward(self, tokenizer, images, prompt, seeds, cross_attn_heat_maps, **states):
         num_img_exist = max([0]+[int(x.split('-', 1)[0]) for x in os.listdir(self.save_root) if x.rsplit('.', 1)[-1] in types_support])
 
         for bid, (p, img) in enumerate(zip(prompt, images)):
             img_path = os.path.join(self.save_root, f"{num_img_exist}-{seeds[bid]}-cross_attn-{to_validate_file(prompt[0])}.{self.image_type}")
-            img = self.draw_attn(memory.tokenizer, p, img, cross_attn_heat_maps[bid])
+            img = self.draw_attn(tokenizer, p, img, cross_attn_heat_maps[bid])
             img.save(img_path, quality=self.quality)
             num_img_exist += 1
