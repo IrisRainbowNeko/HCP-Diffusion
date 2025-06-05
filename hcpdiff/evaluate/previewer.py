73d81a94e865023519cae9d998425ed9b3457777
IrisRainbowNeko <228218809@qq.com>
2025-06-05 20:59:16 +0800

    update

--- hcpdiff/evaluate/previewer.py
+++ hcpdiff/evaluate/previewer.py
@@ -8,10 +8,19 @@ from hcpdiff.models.wrapper import SD15Wrapper
 from accelerate.hooks import remove_hook_from_module
 
 class HCPPreviewer(WorkflowPreviewer):
+    def __init__(self, parser, cfgs_raw, workflow: str | ModuleType | Dict, ds_name=None, interval=100, trainer=None,
+                 mixed_precision=None, seed=42, **cfgs):
+        super().__init__(parser, cfgs_raw, workflow, ds_name=ds_name, interval=interval, trainer=trainer,
+                         mixed_precision=mixed_precision, seed=seed, **cfgs)
+        if trainer is None:
+            self.pt_trainable = False
+        else:
+            self.emb_pt = trainer.cfgs.emb_pt
+            self.pt_trainable = trainer.pt_trainable
 
     @torch.no_grad()
     def evaluate(self, step: int, prefix='eval/'):
-        if step%self.interval != 0 or not self.trainer.is_local_main_process:
+        if step%self.interval != 0 or not self.is_local_main_process:
             return
 
         # record training layers
@@ -23,21 +32,21 @@ class HCPPreviewer(WorkflowPreviewer):
             training_layers = []
             model = None
 
-        model.eval()
-        self.trainer.loggers.info(f'Preview')
+        if self.loggers is not None:
+            self.loggers.info(f'Preview')
 
         N_repeats = model.text_enc_hook.N_repeats
         clip_skip = model.text_enc_hook.clip_skip
         clip_final_norm = model.text_enc_hook.clip_final_norm
         use_attention_mask = model.text_enc_hook.use_attention_mask
 
-        preview_root = Path(self.trainer.exp_dir)/'imgs'
+        preview_root = Path(self.exp_dir)/'imgs'
         preview_root.mkdir(parents=True, exist_ok=True)
 
         states = self.workflow_runner.run(model=model, in_preview=True, te_hook=model.text_enc_hook,
                                           device=self.device, dtype=self.dtype, preview_root=preview_root, preview_step=step,
-                                          world_size=self.trainer.world_size, local_rank=self.trainer.local_rank,
-                                          emb_hook=self.trainer.cfgs.emb_pt.embedding_hook if self.trainer.pt_trainable else None)
+                                          world_size=self.world_size, local_rank=self.local_rank,
+                                          emb_hook=self.emb_pt.embedding_hook if self.pt_trainable else None)
 
         # restore model states
         if model.vae is not None:
@@ -48,11 +57,11 @@ class HCPPreviewer(WorkflowPreviewer):
                 model.vae.encode = states['vae_encode_raw']
                 model.vae.decode = states['vae_decode_raw']
 
-        if 'emb_hook' in states and not self.trainer.pt_trainable:
+        if 'emb_hook' in states and not self.pt_trainable:
             states['emb_hook'].remove()
 
-        if self.trainer.pt_trainable:
-            self.trainer.cfgs.emb_pt.embedding_hook.N_repeats = N_repeats
+        if self.pt_trainable:
+            self.emb_pt.embedding_hook.N_repeats = N_repeats
 
         model.tokenizer.N_repeats = N_repeats
         model.text_enc_hook.N_repeats = N_repeats
