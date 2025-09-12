3c19dd0193bf0361bcbff4596a3e6e03eca8b847
7小时的萌薪 <101446464+xiningin@users.noreply.github.com>
2025-09-12 11:01:30 +0800

    Update base.py

--- hcpdiff/diffusion/sampler/base.py
+++ hcpdiff/diffusion/sampler/base.py
@@ -121,10 +121,8 @@ class Sampler(BaseSampler):
         d_alpha, d_sigma = self.sigma_scheduler.velocity(t)
         if eps is None:
             eps = self.x0_to_eps(x_0, x_t, t)
-            
-        num_dim_to_expand = len(x_0.shape) - 1
-        d_alpha = d_alpha.view(-1, *[1]*num_dim_to_expand)
-        d_sigma = d_sigma.view(-1, *[1]*num_dim_to_expand)
+        d_alpha = (d_alpha,x_0.ndim-1).to(x_0.device)
+        d_sigma = (d_sigma,eps.ndim-1).to(eps.device)
         return d_alpha*x_0+d_sigma*eps
 
     def eps_to_x0(self, eps, x_t, t):
